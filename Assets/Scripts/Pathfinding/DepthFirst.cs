df3b38649e1dba2fa9ee5129b5f20ec7e113796f
Mattias Stjernstedt <mstjernstedt@gmail.com>
2016-01-30 17:39:51 +0900

    getting grid with depth first search

---
+++ Assets/Scripts/Pathfinding/DepthFirst.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+// class that explores first child of every node until it runs out of moves
+// then goes to the next child and does the same until no nodes are left
+public class DepthFirst : MonoBehaviour
+{
+	Dictionary<Vector3, Node> hexes;
+	Dictionary<Vector3, Vector3> cameFrom = new Dictionary<Vector3, Vector3>();
+	Dictionary<Vector3, int> costSoFar = new Dictionary<Vector3, int>();
+
+	Vector3 start = new Vector3(0, 0, 0);
+
+	int moved = 0;
+	public int maxMoves = 5;
+
+	// Use this for initialization
+	void Awake()
+	{
+		hexes = GameObject.Find("World Data").GetComponent<CreateMap>().hexes;
+	}
+
+	public void GetGrid(Vector3 start)
+	{
+		cameFrom[start] = start;
+		costSoFar[start] = 0;
+		moved = 0;
+		GoDeeper(start);
+	}
+
+	void GoDeeper(Vector3 currentCell)
+	{
+		moved += 1;
+		foreach (Vector3 neighbour in hexes[currentCell].neighbours)
+		{
+			if (hexes.ContainsKey(neighbour))
+			{
+				if (hexes[neighbour].walkable)
+				{
+					int newCost = costSoFar[currentCell] + hexes[neighbour].cost;
+					if (!costSoFar.ContainsKey(neighbour) || newCost < costSoFar[neighbour])
+					{
+						cameFrom[neighbour] = currentCell;
+
+						costSoFar[neighbour] = newCost;
+						if (moved <= maxMoves)
+						{
+							GoDeeper(neighbour);
+						}
+						ColorHex(neighbour);
+					}
+				}
+			}
+		}
+		moved -= 1;
+
+	}
+
+	void ColorHex(Vector3 hex)
+	{
+		if (costSoFar[hex] <= 2)
+			hexes[hex].GetComponent<MeshRenderer>().material.color = Color.green;
+		if (costSoFar[hex] > 2 && costSoFar[hex] <= 4)
+			hexes[hex].GetComponent<MeshRenderer>().material.color = Color.yellow;
+		if (costSoFar[hex] > 4 && costSoFar[hex] <= 5)
+			hexes[hex].GetComponent<MeshRenderer>().material.color = Color.red;
+	}
+
+	public void Reset()
+	{
+		foreach (Vector3 hex in cameFrom.Keys)
+		{
+			hexes[hex].GetComponent<MeshRenderer>().material.color = Color.white;
+		}
+
+		costSoFar.Clear();
+		cameFrom.Clear();
+	}
+
+	public Stack<Node> GetPath(Vector3 start, Vector3 dest)
+	{
+		// TODO add checks if destination is in range
+
+		Stack<Node> path = new Stack<Node>();
+		path.Push(hexes[dest]);
+
+		Vector3 current = dest;
+		while (current != start)
+		{
+			path.Push(hexes[cameFrom[current]]);
+			current = cameFrom[current];
+		}
+
+		return path;
+	}
+}
--- Assets/Scripts/PlayerHandler.cs
+++ Assets/Scripts/PlayerHandler.cs
@@ -9,14 +9,16 @@ public class PlayerHandler : MonoBehaviour
 	public GameObject playerPrefab;
 	GameObject player;
 	GameObject enemy;
-	GameObject selected;
+	public GameObject selected;
 	public Vector3 startingPos = new Vector3(0, 0, 0);
 	public Vector3 enemyStartingPos = new Vector3(-2, 11, -9);
 	MouseHandler mouseHandler;
 	AStar astar;
+	DepthFirst depthFirst;
 
 	Vector3 currentCell;
-	List<Node> path;
+	//List<Node> path;
+	Stack<Node> path;
 	Node dest;
 	Node oldDest;
 	public float walkSpeed = 0.5f;
@@ -34,11 +36,17 @@ public class PlayerHandler : MonoBehaviour
 	public Color mediumColor;
 	public Color farColor;
 
+
+	void Awake()
+	{
+	}
+
 	// Use this for initialization
 	void Start()
 	{
 		mouseHandler = GameObject.Find("World Data").GetComponent<MouseHandler>();
 		astar = GameObject.Find("World Data").GetComponent<AStar>();
+		depthFirst = GameObject.Find("World Data").GetComponent<DepthFirst>();
 		hexes = GameObject.Find("World Data").GetComponent<CreateMap>().hexes;
 		float startingHeight = hexes[startingPos].GetComponent<MeshRenderer>().bounds.max.y;
 		float enemyStartingHeight = hexes[enemyStartingPos].GetComponent<MeshRenderer>().bounds.max.y;
@@ -48,6 +56,8 @@ public class PlayerHandler : MonoBehaviour
 		enemy.name = "Enemy";
 		player.transform.position = startingPos + new Vector3(0, startingHeight, 0);
 		enemy.transform.position = hexes[enemyStartingPos].transform.position + new Vector3(0, enemyStartingHeight, 0);
+		SelectUnit(player);
+
 	}
 
 	// Update is called once per frame
@@ -58,42 +68,45 @@ public class PlayerHandler : MonoBehaviour
 			if (Input.GetMouseButtonDown(1))
 			{
 				if (!walking)
-					currentCell = GetCurrentCell().coord;
-			}
-			if (Input.GetMouseButton(1))
-			{
-				if (!walking)
 				{
-					oldDest = dest;
-					dest = mouseHandler.FindCell();
-					if (dest != null && !dest.Equals(oldDest))
-					{
-						path = astar.CalculatePath(currentCell, dest.coord);
-					}
-					for (int i = 0; i < path.Count; i++)
-					{
-						if (i <= shortRange)
-							hexes[path[i].coord].GetComponent<MeshRenderer>().material.color = shortColor;
-						if (i > shortRange && i <= mediumRange)
-							hexes[path[i].coord].GetComponent<MeshRenderer>().material.color = mediumColor;
-						if (i > mediumRange)
-							hexes[path[i].coord].GetComponent<MeshRenderer>().material.color = farColor;
-					}
+					currentCell = GetCurrentCell().coord;
+					path = depthFirst.GetPath(currentCell, mouseHandler.FindCell().coord);
+					StartCoroutine(Walk());
 				}
 			}
+			//if (Input.GetMouseButton(1))
+			//{
+			//	if (!walking)
+			//	{
+			//		oldDest = dest;
+			//		dest = mouseHandler.FindCell();
+			//		if (dest != null && !dest.Equals(oldDest))
+			//		{
+			//			path = astar.CalculatePath(currentCell, dest.coord);
+			//		}
+			//		for (int i = 0; i < path.Count; i++)
+			//		{
+			//			if (i <= shortRange)
+			//				hexes[path[i].coord].GetComponent<MeshRenderer>().material.color = shortColor;
+			//			if (i > shortRange && i <= mediumRange)
+			//				hexes[path[i].coord].GetComponent<MeshRenderer>().material.color = mediumColor;
+			//			if (i > mediumRange)
+			//				hexes[path[i].coord].GetComponent<MeshRenderer>().material.color = farColor;
+			//		}
+			//	}
+			//}
 
-			if (Input.GetMouseButtonUp(1))
-			{
-				if (!walking && dest != null)
-					StartCoroutine(Walk());
-				astar.resetPath();
-				temp++;
-
-			}
+			//if (Input.GetMouseButtonUp(1))
+			//{
+			//	if (!walking && dest != null)
+			//		StartCoroutine(Walk());
+			//	astar.resetPath();
+			//	temp++;
+			//}
 		}
 	}
 
-	Node GetCurrentCell()
+	public Node GetCurrentCell()
 	{
 		Node node = null;
 		Ray ray = new Ray(selected.transform.position, new Vector3(0, -1, 0));
@@ -110,15 +123,15 @@ public class PlayerHandler : MonoBehaviour
 	{
 		walking = true;
 
-		for (int i = 0; i < path.Count; i++)
-		{
+		//for (int i = 0; i < path.Count; i++)
+		//{
 
-			Node node = path[i];
-			float cellHeight = hexes[node.coord].GetComponent<MeshRenderer>().bounds.max.y;
-			selected.transform.position = node.transform.position + new Vector3(0, cellHeight, 0);
-			yield return new WaitForSeconds(walkSpeed);
+		//	Node node = path[i];
+		//	float cellHeight = hexes[node.coord].GetComponent<MeshRenderer>().bounds.max.y;
+		//	selected.transform.position = node.transform.position + new Vector3(0, cellHeight, 0);
+		//	yield return new WaitForSeconds(walkSpeed);
 
-		}
+		//}
 		//while (path.Count > 0 && selected.GetComponent<Mech>().moves > 0)
 		//{
 		//	Node node = path.Pop();
@@ -126,6 +139,16 @@ public class PlayerHandler : MonoBehaviour
 		//	selected.transform.position = node.transform.position + new Vector3(0, cellHeight, 0);
 		//	yield return new WaitForSeconds(walkSpeed);
 		//}
+		while (path.Count > 0)
+		{
+			Node node = path.Pop();
+			float cellHeight = hexes[node.coord].GetComponent<MeshRenderer>().bounds.max.y;
+			selected.transform.position = node.transform.position + new Vector3(0, cellHeight, 0);
+			yield return new WaitForSeconds(walkSpeed);
+		}
+
+		depthFirst.Reset();
+		depthFirst.GetGrid(GetCurrentCell().coord);
 		path.Clear();
 		walking = false;
 	}
@@ -134,11 +157,14 @@ public class PlayerHandler : MonoBehaviour
 	{
 		if (selected != null)
 		{
+			depthFirst.Reset();
 			selected.GetComponent<MeshRenderer>().material.color = Color.white;
 		}
 		selected = unit;
 		selected.GetComponent<MeshRenderer>().material.color = Color.cyan;
 		PopulateActionsPanel();
+		depthFirst.GetGrid(GetCurrentCell().coord);
+
 	}
 
 	void PopulateActionsPanel()
@@ -146,7 +172,7 @@ public class PlayerHandler : MonoBehaviour
 		GameObject panel = GameObject.Find("ActionsPanel");
 		foreach (Transform button in panel.transform)
 		{
-			Destroy(button);
+			Destroy(button.gameObject);
 		}
 
 		foreach (Action action in selected.GetComponents<Action>())
